15918074daf64edb1b05287f6a8d5d1fd8eccbbf
Abdu Maa <abdurahmanmaa@hotmail.de>
2020-12-31 21:53:05 +0100

    Added python script

---
+++ pred.py
@@ -0,0 +1,12 @@
+# Data Manipulation
+import pandas as pd
+import numpy as np
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as pgo
+import plotly.express as px
+
+# Load in Data
+df = pd.read_csv('./Lab5_PredictionChallenge_training.csv')
