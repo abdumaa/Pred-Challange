80bdfae7a230c6f69db439a34e7cbfe538706d9b
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-01-01 18:16:15 +0100

    Added data understanding, visualization to script

--- pred.py
+++ pred.py
@@ -10,3 +10,22 @@ import plotly.express as px
 
 # Load in Data
 df = pd.read_csv('./pred-challenge/Lab5_PredictionChallenge_training.csv')
+
+# Understanding the data
+df.head()
+df.shape # 39241 x 13
+df.columns 
+df.dtypes # All variables are float or int
+df.isnull().sum() # No missing values
+
+# Distribution of features
+def Show_dist(feat):
+    if "dum" in feat:
+        sns.countplot(x = feat, data = df)
+        plt.show()
+    else:
+        sns.histplot(df[feat])
+        plt.show()
+
+Show_dist('dum_floorplan')
+
