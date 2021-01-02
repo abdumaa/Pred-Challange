9b52c7a2eec339ab6d960f6c3bd5c1f149d0ea15
Abdu Maa <abdurahmanmaa@hotmail.de>
2021-01-02 18:55:49 +0100

    added correlation matrix and feat engineering

--- pred.py
+++ pred.py
@@ -27,5 +27,30 @@ def Show_dist(feat):
         sns.histplot(df[feat])
         plt.show()
 
-Show_dist('dum_floorplan')
+Show_dist('dum_privateoffer')
+
+# Correlation matrix using heatmap
+sns.set(style = "white")
+cor_matrix = df.loc[:, 'Rent':].corr()
+
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(cor_matrix, dtype = np.bool))
+
+plt.figure(figsize = (15, 12))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap = True)
+
+sns.heatmap(cor_matrix, mask = mask, cmap = cmap, center = 0,
+            square = True, linewidths = .5, cbar_kws = {"shrink": .5})
+plt.show()
+
+# Feature Engineering
+# Split up postcode in regions (first 2 digits) and areas (last 3 digits)
+v = df.postcode.astype(str)
+df['region'], df['area'] = v.str[:2], v.str[2:]
+df = df.drop(['postcode'], axis = 1)
+df['region'] = df['region'].astype(int)
+df['area'] = df['area'].astype(int)
+
 
